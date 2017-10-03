5665d66dcc0f59f64d4d2a97045d8d207c404bf1
Helgefy <helge_fylkesnes@hotmail.com>
2017-10-03 11:07:41 +0200

    hei

--- siteapp/models.py
+++ siteapp/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from ckeditor.fields import RichTextField, 
+from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
